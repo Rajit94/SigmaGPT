4c87d48918d520862bb915b914f1a235e6f444f1
Rajit94 <baghelrajit94@gmail.com>
2025-12-23 10:06:38 +0530

    set-up completed

---
+++ Backend/routes/chat.js
@@ -0,0 +1,21 @@
+import express from "express";
+import Thread from "../models/Thread.js";
+
+const router = express.Router();
+
+router.post("/test", async(req, res) => {
+    try {
+        const thread = new Thread({
+            threadId: "xyz",
+            title: "Testing New Thread"
+        });
+
+        const response = await thread.save();
+        res.send(response);
+    } catch(err) {
+        console.log(err);
+        res.status(500).json({error: "failed to save in Db"});
+    }
+});
+
+export default router;
--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import "dotenv/config";
 import cors from "cors";
+import mongoose from "mongoose";
+import chatRoutes from "./routes/chat.js"
 
 
 const app = express();
@@ -9,35 +11,18 @@ const PORT = 8080;
 app.use(express.json());
 app.use(cors());
 
+app.use("/api", chatRoutes);
+
 app.listen(PORT, () => {
     console.log(`server running on ${PORT}`);
+    connectDB();
 });
 
-app.post("/test", async (req, res) => {
-const options = {
-method: "POST",
-headers: {
-"Content-Type": "application/json",
-"Authorization": `Bearer ${process.env.OPEΝΑΙ_API_KEY}`
-},
-
-body: JSON.stringify({
-model: "gpt-40-mini",
-messages: [{
-role: "user",
-content: req.body.message
-}]
-})
-};
-
-try {
-const response = await fetch("https://api.openai.com/v1/chat/completions", options);
-const data = await response.json();
-
-//console.log(data.choices[0].message.content); //reply
-
-res.send(data.choices[0].message.content);
-} catch(err) {
-console.log(err);
+const connectDB = async() => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URI);
+        console.log("Connected with Database!");
+    } catch(err) {
+        console.log("failed to connect with Db", err);
+    }
 }
-});
