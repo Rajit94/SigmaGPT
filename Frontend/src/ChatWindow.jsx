bf7e7f172d69660ed9daec9a5305e6e1efcb6d75
Rajit Baghel <baghelrajit94@gmail.com>
2025-12-26 19:51:13 +0530

    build & Styled chat+sidebar

--- Frontend/src/ChatWindow.jsx
+++ Frontend/src/ChatWindow.jsx
@@ -1,10 +1,32 @@
 import "./ChatWindow.css";
+import Chat from "./Chat.jsx";
+
+
 function ChatWindow() {
     return (
-        <div>
-            Chatwindow
+        <div className="chatWindow"> 
+        <div className="navbar">
+            <span>SigmaGPT <i class="fa-solid fa-chevron-down"></i></span>
+            <div className="userIconDiv">
+            <span><i class="fa-solid fa-user"></i></span>
         </div>
+        </div>
+        
+        <Chat></Chat>
+        <div className="chatInput">
+            <div className="chatInput">
+         <input placeholder="Ask anything">
+         </input>
+         <div id="submit"><i class="fa-solid fa-paper-plane"></i></div>
+        </div>
+        <p className="info">
+         SigmaGPT can make mistakes. Check important info. See Cookie Preferences.
+        </p>
+        </div>
+        </div>
+        
     )
 }
+
 
 export default ChatWindow;
--- Frontend/src/Sidebar.jsx
+++ Frontend/src/Sidebar.jsx
@@ -1,9 +1,23 @@
 import "./Sidebar.css";
 function Sidebar() {
     return (
-        <div>
-            Sidebar
-        </div>
+      <section className="sidebar">
+       <button>
+        <img src="src/assets/blacklogo.png" alt="gpt logo" className="logo"></img>
+        <span><i className="fa-solid fa-pen-to-square"></i></span>
+       </button>
+
+       <ul className="history">
+        <ul>thread1</ul>
+       <ul>thread2</ul>
+       <ul>thread3</ul>
+       </ul>
+
+       <div className="sign">
+          <p> By Rajit &hearts; </p>
+       </div>
+       
+      </section>
     )
 }
 
