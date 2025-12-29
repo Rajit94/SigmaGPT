814bbc09b7e5eaa65bec190772ca0fbfbe4faeea
Rajit Baghel <baghelrajit94@gmail.com>
2025-12-29 18:49:19 +0530

    working on display chat

--- Frontend/src/Chat.jsx
+++ Frontend/src/Chat.jsx
@@ -8,12 +8,18 @@ function Chat() {
         <>
         {newChat && <h1>Start a New Chat</h1>}
         <div className="chats">
-        <div className="userDiv">
-            <p className="userMessage">User Message</p>
-        </div>
-        <div className="gptDiv">
-            <p className="gptMessage">GPT Generated Message</p>
-        </div>
+            {
+                prevChats?.map((chat, idx) => 
+                    <div className={chat.role === "user"? "userDiv" : "gptDiv"} key={idx}>
+                        {
+                            chat.role === "user"?
+                            <p className="userMessage">{chat.content}</p> :
+                            <p className="gptMessage">{chat.content}</p>
+                        }
+                    </div>   
+                    )
+                }
+
         </div>
         </>
     )
--- Frontend/src/ChatWindow.jsx
+++ Frontend/src/ChatWindow.jsx
@@ -1,16 +1,18 @@
 import "./ChatWindow.css";
 import Chat from "./Chat.jsx";
 import { MyContext } from "./MyContext.jsx";
-import { useContext, useState } from "react";
+import { useContext, useState, useEffect } from "react";
 import { ScaleLoader } from "react-spinners";
 
 
+
 function ChatWindow() {
-    const {prompt, setPrompt, reply, setReply, currThreadId} = useContext(MyContext);
+    const {prompt, setPrompt, reply, setReply, currThreadId, setPrevChats} = useContext(MyContext);
     const [loading, setLoading] = useState(false);
 
     const getReply = async () => {
         setLoading(true);
+
         const options = {
             method: "POST",
             headers: {
@@ -32,6 +34,24 @@ function ChatWindow() {
         }
         setLoading(false);
     }
+
+    //append new chat to prev chats 
+    useEffect(() => {
+       if(prompt && reply) {
+        setPrevChats(prevChats => (
+                [...prevChats, {
+                    role: "user",
+                    content: prompt
+                },{
+                    role: "assistant",
+                    content: reply
+                }]
+            ));
+        }
+
+        setPrompt("");
+    }, [reply]);
+
     return (
         <div className="chatWindow"> 
         <div className="navbar">
