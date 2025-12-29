859068c3070806c1282bb8707ce0877c419ece61
Rajit94 <baghelrajit94@gmail.com>
2025-12-29 10:06:52 +0530

    creating chats window reply functionality

--- Frontend/src/Chat.jsx
+++ Frontend/src/Chat.jsx
@@ -1,7 +1,21 @@
 import "./Chat.css";
+import { useContext } from "react";
+import { MyContext } from "./MyContext";
+
 function Chat() {
+    const {newChat, prevChats} = useContext(MyContext);
     return (
-        <></>
+        <>
+        {newChat && <h1>Start a New Chat</h1>}
+        <div className="chats">
+        <div className="userDiv">
+            <p className="userMessage">User Message</p>
+        </div>
+        <div className="gptDiv">
+            <p className="gptMessage">GPT Generated Message</p>
+        </div>
+        </div>
+        </>
     )
 }
 
--- Frontend/src/ChatWindow.jsx
+++ Frontend/src/ChatWindow.jsx
@@ -1,32 +1,66 @@
 import "./ChatWindow.css";
 import Chat from "./Chat.jsx";
+import { MyContext } from "./MyContext.jsx";
+import { useContext, useState } from "react";
+import { ScaleLoader } from "react-spinners";
 
 
 function ChatWindow() {
+    const {prompt, setPrompt, reply, setReply, currThreadId} = useContext(MyContext);
+    const [loading, setLoading] = useState(false);
+
+    const getReply = async () => {
+        setLoading(true);
+        const options = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                message: prompt,
+                threadId: currThreadId
+            })
+        };
+
+        try {
+            const response = await fetch("http://localhost:8080/api/chat", options);
+            const res = await response.json();
+            console.log(res);
+            setReply(res.reply);
+        } catch(err) {
+            console.log(err);
+        }
+        setLoading(false);
+    }
     return (
         <div className="chatWindow"> 
         <div className="navbar">
-            <span>SigmaGPT <i class="fa-solid fa-chevron-down"></i></span>
+            <span>SigmaGPT <i className="fa-solid fa-chevron-down"></i></span>
             <div className="userIconDiv">
-            <span><i class="fa-solid fa-user"></i></span>
+            <span><i className="fa-solid fa-user"></i></span>
         </div>
         </div>
         
         <Chat></Chat>
-        <div className="chatInput">
-            <div className="chatInput">
-         <input placeholder="Ask anything">
-         </input>
-         <div id="submit"><i class="fa-solid fa-paper-plane"></i></div>
+        <ScaleLoader color="#fff" loading={loading}>
+        </ScaleLoader>
+         <div className="chatInput">
+                <div className="inputBox">
+                    <input placeholder="Ask anything"
+                        value={prompt}
+                        onChange={(e) => setPrompt(e.target.value)}
+                        onKeyDown={(e) => e.key === 'Enter'? getReply() : ''}
+                    >
+                           
+                    </input>
+                    <div id="submit" onClick={getReply}><i className="fa-solid fa-paper-plane"></i></div>
+                </div>
+                <p className="info">
+                    SigmaGPT can make mistakes. Check important info. See Cookie Preferences.
+                </p>
+            </div>
         </div>
-        <p className="info">
-         SigmaGPT can make mistakes. Check important info. See Cookie Preferences.
-        </p>
-        </div>
-        </div>
-        
     )
 }
-
 
 export default ChatWindow;
